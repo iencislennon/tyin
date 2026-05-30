f19a04e93bc4ecd36ed0644fccd7b2b8128a7041
Ansar Tleubayev <ansartleubayev@MacBook-Air-Ansar.local>
2026-05-30 08:32:18 -0400

    remove logging file for raileay

--- agent/pipeline.py
+++ agent/pipeline.py
@@ -5,7 +5,6 @@ from database.models import save_message, get_history
 import logging
 
 logging.basicConfig(
-    filename="logs/app.log",
     level=logging.INFO,
     format="%(asctime)s %(levelname)s %(message)s"
 )
